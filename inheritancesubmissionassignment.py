a085e287bf190e079d49ad63814c03b97a6f1b48
StevenFralix <80490144+steveaf7@users.noreply.github.com>
2021-05-18 15:20:59 -0700

    Inheritance Submission Assignment
    create two classes that inherit properties from a parent class and also have their own properties

---
+++ inheritancesubmissionassignment.py
@@ -0,0 +1,25 @@
+
+#create class Instrument
+class Instrument:
+    type = '' 
+    color = ''
+    size = ''
+    brand = ''
+
+#create class Drum, child of class Instrument
+class Drum(Instrument):
+    top-head = ''
+    bottom-head = ''
+    type_of_wood = '' 
+
+#create class guitar, inherits properties from instrument class
+class Guitar(Instrument):
+    number_of_strings: 0
+    type_of_strings: ''
+    type_of_wood: ''
+    
+    
+
+    
+    
+    
